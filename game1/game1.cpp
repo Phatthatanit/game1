6f5b8fd617f5557fb3affb164976921434bad09a
PHATTHARANIT TESJAROEN <63010727@KMITL.AC.TH>
2020-11-01 18:26:04 +0700

    finish01

--- game1/game1.cpp
+++ game1/game1.cpp
@@ -1,11 +1,55 @@
-
+#define _CRT_SECURE_NO_WARNINGS
 #include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
 #include <iostream>
 #include <windows.h>
+#include<stdio.h>
+#include<conio.h>
+char ch = '.';
+int go = 0, x = 38, y = 20;
+
+void gotoxy(int x, int y)
+{
+	COORD c = { x, y };
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), c);
+}
+
+void setcolor(int fg, int bg)
+{
+	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+	SetConsoleTextAttribute(hConsole, bg * 16 + fg);
+}
+
+void erase_ship(int x, int y) {
+	gotoxy(x, y);
+	setcolor(2, 0);
+	printf("       ");
+}
+
+void draw_ship(int x, int y)
+{
+	gotoxy(x, y);
+	setcolor(2, 4);
+	printf("<-0->");
+}
 
 int main()
 {
-	sf::RenderWindow window(sf::VideoMode(1475, 600), "Game from scratch!");
+
+	sf::RenderWindow window(sf::VideoMode(1475, 420), "Game from scratch!" );
+	sf::View view(sf::Vector2f(0.0f, 0.0f), sf::Vector2f(1475.0f, 420.0f));
+
+	sf::SoundBuffer buffer;
+	if (!buffer.loadFromFile("pic/s3.ogg"))
+	{
+		std::cout << "Load failed" << std::endl;
+	}
+
+	sf::Sound sound;
+	sound.setLoop(true);
+	sound.setBuffer(buffer);
+	sound.play();
+	
 
 	////// Circle
 	sf::CircleShape collision(40.f);
@@ -18,22 +62,33 @@ int main()
 	{
 		std::cout << "Load failed" << std::endl;
 	}
-
-	
-	////// Sprite
-	/*sf::Sprite shapeSprite;
-	shapeSprite.setTexture(playerTexture);
-	shapeSprite.setTextureRect(sf::IntRect(0, 0 ,148 ,350)); */
-
 	sf::Sprite shapeSprite;
 	shapeSprite.setTexture(playerTexture);
-
-	int spriteSizeX = playerTexture.getSize().x / 10 ;
+	int spriteSizeX = playerTexture.getSize().x / 10;
 	int spriteSizeY = playerTexture.getSize().y / 1;
 	shapeSprite.setTextureRect(sf::IntRect(0, 0, spriteSizeX, spriteSizeY));
+	
+	shapeSprite.setOrigin(120.0f / 2.0f , 125.0f / 2.0f);
 
-	sf::Vector2f spawnPoint = { 0.f, 0.f };
+	////// bg01
+	sf::Texture bgTexture;
+	if (!bgTexture.loadFromFile("pic/bg01.png"))
+	{
+		std::cout << "Load failed" << std::endl;
+	}
+
+	sf::RectangleShape bg(sf::Vector2f(10000.0f, 420.0f));
+	bgTexture.loadFromFile("pic/1bg.png");
+	bg.setTexture(&bgTexture);
+	
+	
+
+	//sf::Vector2f Getposition() {return shapeSprite.getposition(); }
+			
+
+	sf::Vector2f spawnPoint = { 1200.f, 210.f };
 	shapeSprite.setPosition(spawnPoint);
+
 
 	int animationFrame = 0;
 	float totalTime = 0;
@@ -43,54 +98,60 @@ int main()
 
 	while (window.isOpen())
 	{
+		view.setCenter(shapeSprite.getPosition().x-400.0f,210.0f);
+
 		deltaTime = clock.restart().asSeconds();
+		window.setView(view);
+		window.draw(bg);
 		window.draw(shapeSprite);
 		window.draw(collision);
 		window.display();
+	
 		
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
-		{
-			shapeSprite.move(speed*deltaTime, 0.f*speed );
-			shapeSprite.setTextureRect(sf::IntRect(spriteSizeX * animationFrame, spriteSizeY * 0, 120, 125));
 
-			
-		}
-		/*if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
-		{
-			shapeSprite.move(-.1f, 0.f);
-			shapeSprite.setTextureRect(sf::IntRect(spriteSizeX * animationFrame, spriteSizeY * 3, spriteSizeX, spriteSizeY));
-		} */
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
-		{
-			shapeSprite.move(0.f*speed, -speed * deltaTime);
-			shapeSprite.setTextureRect(sf::IntRect(spriteSizeX * animationFrame, spriteSizeY * 0, 120, 125));
-		}
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
-		{
-			shapeSprite.move(0.f*speed, speed * deltaTime);
-			shapeSprite.setTextureRect(sf::IntRect(spriteSizeX * animationFrame, spriteSizeY * 0, 120, 125));
-		}
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
+			{
+			window.close();
+			}
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
+			{
+				shapeSprite.move(speed * deltaTime, 0.f * speed);
+				shapeSprite.setTextureRect(sf::IntRect(spriteSizeX * animationFrame, spriteSizeY * 0, 120, 125));
+			}
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+			{
+				shapeSprite.move(-speed * deltaTime, 0.f * speed);
+				shapeSprite.setTextureRect(sf::IntRect(spriteSizeX * animationFrame, spriteSizeY * 0, 120, 125));
+			}
+
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
+			{
+				shapeSprite.move(0.f * speed, -speed * deltaTime);
+				shapeSprite.setTextureRect(sf::IntRect(spriteSizeX * animationFrame, spriteSizeY * 0, 120, 125));
+			}
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
+			{
+				shapeSprite.move(0.f * speed, speed * deltaTime);
+				shapeSprite.setTextureRect(sf::IntRect(spriteSizeX * animationFrame, spriteSizeY * 0, 120, 125));
+			}
+		
 		if (collision.getGlobalBounds().intersects(shapeSprite.getGlobalBounds())) {
 			Beep(329, 100);
 			Beep(493, 100);
 			shapeSprite.setPosition(spawnPoint);
-			
+
 		}
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
-		{
-			window.close();
-		}
+		
 		totalTime += deltaTime;
 		if (totalTime >= 0.1)
 		{
 			totalTime -= 0.1;
 			animationFrame++;
 		}
-		 
+
 		if (animationFrame >= 9) {
 			animationFrame = 0;
 		}
-
 		window.clear();
 	}
 	return 0;
