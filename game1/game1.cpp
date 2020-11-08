34ae406912ac982c80f3348537d86f3543b2a786
PHATTHARANIT TESJAROEN <63010727@KMITL.AC.TH>
2020-11-08 16:07:58 +0700

    a

--- game1/game1.cpp
+++ game1/game1.cpp
@@ -5,38 +5,11 @@
 #include <windows.h>
 #include<stdio.h>
 #include<conio.h>
-char ch = '.';
-int go = 0, x = 38, y = 20;
-
-void gotoxy(int x, int y)
-{
-	COORD c = { x, y };
-	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), c);
-}
-
-void setcolor(int fg, int bg)
-{
-	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-	SetConsoleTextAttribute(hConsole, bg * 16 + fg);
-}
-
-void erase_ship(int x, int y) {
-	gotoxy(x, y);
-	setcolor(2, 0);
-	printf("       ");
-}
-
-void draw_ship(int x, int y)
-{
-	gotoxy(x, y);
-	setcolor(2, 4);
-	printf("<-0->");
-}
-
+int p = 0;
 int main()
 {
 
-	sf::RenderWindow window(sf::VideoMode(1475, 420), "Game from scratch!" );
+	sf::RenderWindow window(sf::VideoMode(1475, 420), "Game from scratch!");
 	sf::View view(sf::Vector2f(0.0f, 0.0f), sf::Vector2f(1475.0f, 420.0f));
 
 	sf::SoundBuffer buffer;
@@ -49,7 +22,7 @@ int main()
 	sound.setLoop(true);
 	sound.setBuffer(buffer);
 	sound.play();
-	
+
 
 	////// Circle
 	sf::CircleShape collision(40.f);
@@ -67,8 +40,8 @@ int main()
 	int spriteSizeX = playerTexture.getSize().x / 10;
 	int spriteSizeY = playerTexture.getSize().y / 1;
 	shapeSprite.setTextureRect(sf::IntRect(0, 0, spriteSizeX, spriteSizeY));
-	
-	shapeSprite.setOrigin(120.0f / 2.0f , 125.0f / 2.0f);
+
+	shapeSprite.setOrigin(120.0f / 2.0f, 125.0f / 2.0f);
 
 	////// bg01
 	sf::Texture bgTexture;
@@ -80,11 +53,6 @@ int main()
 	sf::RectangleShape bg(sf::Vector2f(10000.0f, 420.0f));
 	bgTexture.loadFromFile("pic/1bg.png");
 	bg.setTexture(&bgTexture);
-	
-	
-
-	//sf::Vector2f Getposition() {return shapeSprite.getposition(); }
-			
 
 	sf::Vector2f spawnPoint = { 1200.f, 210.f };
 	shapeSprite.setPosition(spawnPoint);
@@ -98,7 +66,7 @@ int main()
 
 	while (window.isOpen())
 	{
-		view.setCenter(shapeSprite.getPosition().x-400.0f,210.0f);
+		view.setCenter(shapeSprite.getPosition().x - 400.0f, 210.0f);
 
 		deltaTime = clock.restart().asSeconds();
 		window.setView(view);
@@ -107,41 +75,54 @@ int main()
 		window.draw(collision);
 		window.display();
 	
-		
+		if (shapeSprite.getPosition().x < 10000 && p == 0) {
+		shapeSprite.move(speed* deltaTime, 0.f * speed);
+		shapeSprite.setTextureRect(sf::IntRect(spriteSizeX* animationFrame, spriteSizeY * 0, 120, 125));
+		}
 
-			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
-			{
-			window.close();
-			}
-			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
-			{
-				shapeSprite.move(speed * deltaTime, 0.f * speed);
-				shapeSprite.setTextureRect(sf::IntRect(spriteSizeX * animationFrame, spriteSizeY * 0, 120, 125));
-			}
-			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
-			{
-				shapeSprite.move(-speed * deltaTime, 0.f * speed);
-				shapeSprite.setTextureRect(sf::IntRect(spriteSizeX * animationFrame, spriteSizeY * 0, 120, 125));
-			}
-
-			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
-			{
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) {
+			if (shapeSprite.getPosition().y > 40 && p == 0) {
 				shapeSprite.move(0.f * speed, -speed * deltaTime);
 				shapeSprite.setTextureRect(sf::IntRect(spriteSizeX * animationFrame, spriteSizeY * 0, 120, 125));
 			}
-			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
-			{
+		}
+
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down)) {
+			if (shapeSprite.getPosition().y < 380 && p==0) {
 				shapeSprite.move(0.f * speed, speed * deltaTime);
 				shapeSprite.setTextureRect(sf::IntRect(spriteSizeX * animationFrame, spriteSizeY * 0, 120, 125));
 			}
+		}
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
+		{
+			window.close();
+		}
+
+		if (sf::Mouse::isButtonPressed(sf::Mouse::Right))
+		{
+			if (p == 0) {
+				p = 1;
+				printf("%d", p);
+				Sleep(200);
+			}
+			else {
+				p = 0;
+				printf("%d", p);
+				Sleep(200);
+
+			}
+		}
+
 		
+		
+
 		if (collision.getGlobalBounds().intersects(shapeSprite.getGlobalBounds())) {
 			Beep(329, 100);
 			Beep(493, 100);
 			shapeSprite.setPosition(spawnPoint);
 
 		}
-		
+
 		totalTime += deltaTime;
 		if (totalTime >= 0.1)
 		{
